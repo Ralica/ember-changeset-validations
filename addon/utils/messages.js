d0d2d60f9e5d36af5383084035d90eef21ca88a0
Ralica Momchilova <ralica@momchilovi.com>
2024-08-25 09:29:06 -0400

    a quickie

--- addon/utils/messages.js
+++ addon/utils/messages.js
@@ -1,5 +1,5 @@
 import _Messages from 'ember-validators/messages';
-import { capitalize, dasherize } from '@ember/string';
+// import { capitalize, dasherize } from '@ember/string';
 
 const Messages = Object.assign({}, _Messages);
 
@@ -9,6 +9,9 @@ export default Object.assign(Messages, {
   present: _Messages.blank,
 
   getDescriptionFor(key = '') {
+    const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+    const dasherize = (str) => str.replace(/([a-z\d])([A-Z])/g, '$1-$2').toLowerCase();
+
     return capitalize(dasherize(key).split(/[._-]/g).join(' '));
   },
 });
